7f138e406987f80f6c102397d33c934fecb4adb6
Rhuan Cesar <rhuan.silva@anestesiacarioca.com.br>
2023-02-21 18:17:17 -0300

    Form contato

--- public/pages/contato.php
+++ public/pages/contato.php
@@ -1,1 +1,26 @@
 <h2>Contato</h2>
+<form method="get" action="/pages/forms/contato.php">
+  <div class="form-floating m-1 col-4">
+    <input
+      type="text"
+      class="form-control" name="name" placeholder="">
+    <label for="">nome</label>
+  </div>
+  <div class="form-floating m-1 col-4">
+    <input
+      type="email"
+      class="form-control" name="email" placeholder="">
+    <label for="">Email</label>
+  </div>
+  <div class="form-floating m-1 col-4">
+    <input
+      type="text"
+      class="form-control" name="subject" placeholder="">
+    <label for="">Assunto</label>
+  </div>
+  <div class="form mb-3">
+    <label for="">Menssagem</label>
+    <textarea class="form-control" name="message" rows="10" cols="30"></textarea>
+  </div>
+  <button class="btn btn-primary"type="submit">Enviar</button>
+</form>
