bf59aad6c7089be23a75d29ec3ddc26bac3f14cd
Rhuan Cesar <rhuan.silva@anestesiacarioca.com.br>
2023-02-21 17:43:19 -0300

    Estruturando
    Aula (37, 38, iniciando 39)
    Exception
    getMessage()

---
+++ app/functions/pages.php
@@ -0,0 +1,18 @@
+<?php 
+
+  function load(){
+    
+    $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
+
+    $page = (!$page) ? 'pages/home.php' : "pages/{$page}.php";
+
+    if(!file_exists($page)){
+      
+      throw new \Exception("Alguma coisa de errado aconteceu !");
+    
+    }
+
+    return $page;
+  }
+
+?>
